74a543fa49ffe4e9ca35ce6616abc9f23f3b5410
Fourrageat <baptiste.fourageat@icloud.com>
2023-12-20 20:33:41 +0100

    add swiftLint, fix lint errors

--- CountOnMe/Model/Calculator.swift
+++ CountOnMe/Model/Calculator.swift
@@ -9,27 +9,25 @@
 import Foundation
 
 class Calculator {
-    
     func calculateResult(from elements: [String]) -> String? {
         var operationsToReduce = elements
-        
+
         while operationsToReduce.count > 1 {
             let left = Int(operationsToReduce[0])!
             let operand = operationsToReduce[1]
             let right = Int(operationsToReduce[2])!
-            
+
             let result: Int
             switch operand {
             case "+": result = left + right
             case "-": result = left - right
             default: fatalError("Unknown operator !")
             }
-            
+
             operationsToReduce = Array(operationsToReduce.dropFirst(3))
             operationsToReduce.insert("\(result)", at: 0)
         }
-        
+
         return operationsToReduce.first
     }
-    
 }
