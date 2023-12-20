dcef88ab8e32d24800697d15ad78ea2976ab838a
Fourrageat <baptiste.fourageat@icloud.com>
2023-12-21 00:19:26 +0100

    init first test exemple

---
+++ CountOnMeTests/CalculatorTestCase.swift
@@ -0,0 +1,18 @@
+//
+//  CalculatorTestCase.swift
+//  CountOnMeTests
+//
+//  Created by Baptiste FOURRAGEAT on 21/12/2023.
+//  Copyright © 2023 Vincent Saluzzo. All rights reserved.
+//
+
+import XCTest
+@testable import CountOnMe
+
+final class CalculatorTestCase: XCTestCase {
+
+    func testExample() {
+        XCTAssert(true)
+    }
+
+}
